372b4e9086d3fba3d3281b44cd0d3a5c701d8b76
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-09-08 23:00:31 +0200

    some debugging things

--- wfs/__init__.py
+++ wfs/__init__.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import httplib2
 from unittest import TestCase
 
+log = logging.getLogger(__name__)
+
 class TestWFS(TestCase):
     url = "http://localhost/mapserv-tests?map=/home/sbrunner/workspace/mapserver-wfs-tests/data/test.map&"
+#    url = "http://localhost/mapserv603-tests?map=/home/sbrunner/workspace/mapserver-wfs-tests/data/test.map&"
     http = httplib2.Http()
     
     def _post(self, body):
-        resp, content = http.request(self.url, method='POST', body=body);
+#        log.info(self.url)
+#        log.info(body)
+        response, content = self.http.request(self.url, method='POST', body=body.encode('utf-8'));
         self.assertEquals(int(response['status']), 200)
+#        log.info(content)
         return content
 
     def _assert_result_equals(self, content, value):
-        for test in zip(unicode(value.decode('utf-8')).split('\n'),
-#                unicode(content.decode('utf-8')).split('\n')):
+#        log.info(content)
+#        log.info(value)
+#        for test in zip(unicode(value.decode('utf-8')).split('\n'),
+        for test in zip(value.split('\n'),
                 unicode(content.decode('utf-8')).split('\n')):
-            self.assertEquals(test[0], test[1])
+#            log.info(test[0])
+#            log.info(test[1])
+            if test[0] != 'PASS...':
+                self.assertEquals(test[0], test[1])
