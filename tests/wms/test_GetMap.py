76f9a8e59136d51776b3de2e652e7fab0578190f
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-09-10 15:09:33 +0200

    realy compare image instace of doing an md5

--- tests/wms/test_GetMap.py
+++ tests/wms/test_GetMap.py
@@ -1,30 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import Image
+import ImageChops
+import ImageStat
+from cStringIO import StringIO
 from hashlib import md5
 from tests import TestXML
 
 log = logging.getLogger(__name__)
 
 class TestGetMap(TestXML):
+
+    MAP_0 = Image.open("tests/wms/GetMap-0.png")
+    MAP_1 = Image.open("tests/wms/GetMap-1.png")
+
     def test_GetMap(self):
         QUERY = (
-            { 'layer': 'postgis-point', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'postgis-point', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'postgis-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1387, 'md5': 'ce9b79ded1ec0828d26dc5c6315c5e62' },
-            { 'layer': 'postgis-point', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wms-point', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wms-point', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wms-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1387, 'md5': '93b6d59616ec915ff948501226df363e' },
-            { 'layer': 'wms-point', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wfs-point', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wfs-point', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wfs-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1387, 'md5': 'ce9b79ded1ec0828d26dc5c6315c5e62' },
-            { 'layer': 'wfs-point', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'points', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'points', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'points', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1368, 'md5': 'bb4bd00bf537aec7872ec214bc4fefeb' },
-            { 'layer': 'points', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
+            { 'layer': 'postgis-point', 'bbox': '1,1,3,3', 'result': self.MAP_0 },
+            { 'layer': 'postgis-point', 'bbox': '1.9,1.9,2.1,2.1', 'result': self.MAP_0 },
+            { 'layer': 'postgis-point', 'bbox': '1.99,1.99,2.01,2.01', 'result': self.MAP_1 },
+            { 'layer': 'postgis-point', 'bbox': '1.999,1.999,2.001,2.001', 'result': self.MAP_0 },
+            { 'layer': 'wms-point', 'bbox': '1,1,3,3', 'result': self.MAP_0 },
+            { 'layer': 'wms-point', 'bbox': '1.9,1.9,2.1,2.1', 'result': self.MAP_0 },
+            { 'layer': 'wms-point', 'bbox': '1.99,1.99,2.01,2.01', 'result': self.MAP_1 },
+            { 'layer': 'wms-point', 'bbox': '1.999,1.999,2.001,2.001', 'result': self.MAP_0 },
+            { 'layer': 'wfs-point', 'bbox': '1,1,3,3', 'result': self.MAP_0 },
+            { 'layer': 'wfs-point', 'bbox': '1.9,1.9,2.1,2.1', 'result': self.MAP_0 },
+            { 'layer': 'wfs-point', 'bbox': '1.99,1.99,2.01,2.01', 'result': self.MAP_1 },
+            { 'layer': 'wfs-point', 'bbox': '1.999,1.999,2.001,2.001', 'result': self.MAP_0 },
+            { 'layer': 'points', 'bbox': '1,1,3,3', 'result': self.MAP_0 },
+            { 'layer': 'points', 'bbox': '1.9,1.9,2.1,2.1', 'result': self.MAP_0 },
+            { 'layer': 'points', 'bbox': '1.99,1.99,2.01,2.01', 'result': self.MAP_1 },
+            { 'layer': 'points', 'bbox': '1.999,1.999,2.001,2.001', 'result': self.MAP_0 },
         )
         results = {}
         for q in QUERY:
@@ -42,5 +50,8 @@ class TestGetMap(TestXML):
                 ('SRS', 'EPSG:4326'),
             ))
             self.assertEquals('PNG', content[1:4])
-            self.assertEquals(q['len'], len(content))
-            self.assertEquals(q['md5'], md5(content).hexdigest())
+            img = Image.open(StringIO(content))
+            self.assertEquals(q['result'].size, img.size)
+            diffimg = ImageChops.difference(q['result'], img)
+            stats = ImageStat.Stat(diffimg).var
+            self.assertTrue(stats[0] + stats[1] + stats[2] < 1, "Wrong image")
