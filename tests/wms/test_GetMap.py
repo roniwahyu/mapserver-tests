e53b22172e26890203ddc0795626adfe6417cda2
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-09-10 13:54:55 +0200

    smaller symbols

--- tests/wms/test_GetMap.py
+++ tests/wms/test_GetMap.py
@@ -11,16 +11,20 @@ class TestGetMap(TestXML):
         QUERY = (
             { 'layer': 'postgis-point', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
             { 'layer': 'postgis-point', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'postgis-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 2477, 'md5': 'b8f7974bd616cc3112be222d53b72d7c' },
+            { 'layer': 'postgis-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1387, 'md5': 'ce9b79ded1ec0828d26dc5c6315c5e62' },
             { 'layer': 'postgis-point', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
             { 'layer': 'wms-point', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
             { 'layer': 'wms-point', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wms-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 2480, 'md5': 'e71857deedaaebb3cf6f41aa0cfeca7d' },
+            { 'layer': 'wms-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1387, 'md5': '93b6d59616ec915ff948501226df363e' },
             { 'layer': 'wms-point', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
             { 'layer': 'wfs-point', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
             { 'layer': 'wfs-point', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
-            { 'layer': 'wfs-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 2477, 'md5': 'b8f7974bd616cc3112be222d53b72d7c' },
+            { 'layer': 'wfs-point', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1387, 'md5': 'ce9b79ded1ec0828d26dc5c6315c5e62' },
             { 'layer': 'wfs-point', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
+            { 'layer': 'points', 'bbox': '1,1,3,3', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
+            { 'layer': 'points', 'bbox': '1.9,1.9,2.1,2.1', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
+            { 'layer': 'points', 'bbox': '1.99,1.99,2.01,2.01', 'len': 1368, 'md5': 'bb4bd00bf537aec7872ec214bc4fefeb' },
+            { 'layer': 'points', 'bbox': '1.999,1.999,2.001,2.001', 'len': 1048, 'md5': '64a17ca946e92cbefd2956b0e9663a12' },
         )
         results = {}
         for q in QUERY:
