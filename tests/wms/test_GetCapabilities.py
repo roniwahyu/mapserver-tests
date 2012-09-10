c10db33f0d76801046428221e048c32a1d792cc2
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-09-10 13:12:12 +0200

    add test GetFeature info on group

--- tests/wms/test_GetCapabilities.py
+++ tests/wms/test_GetCapabilities.py
@@ -109,13 +109,77 @@ class TestGetCapabilities(TestXML):
     <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
     <BoundingBox SRS="EPSG:4326"
                 minx="-10" miny="-10" maxx="10" maxy="10" />
-    <Layer queryable="1" opaque="0" cascaded="0">
+    <Layer>
+      <Name>points</Name>
+      <Title>Points group</Title>
+      <Abstract>points</Abstract>
+    <Style>
+       <Name>default</Name>
+       <Title>default</Title>
+       <LegendURL width="65" height="53">
+          <Format>image/png</Format>
+          <OnlineResource xmlns:xlink="http://www.w3.org/1999/xlink" xlink:type="simple" xlink:href="http://example.com/ogc?version=1.1.1&amp;service=WMS&amp;request=GetLegendGraphic&amp;layer=points&amp;format=image/png&amp;STYLE=default"/>
+       </LegendURL>
+    </Style>
+      <Layer queryable="1" opaque="0" cascaded="0">
         <Name>postgis-point-auto</Name>
         <Title>Layer éàè</Title>
         <SRS>EPSG:4326</SRS>
         <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
         <BoundingBox SRS="EPSG:4326"
                     minx="-10" miny="-10" maxx="10" maxy="10" />
+      </Layer>
+      <Layer queryable="1" opaque="0" cascaded="0">
+        <Name>postgis-point</Name>
+        <Title>Layer éàè</Title>
+        <SRS>EPSG:4326</SRS>
+        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
+        <BoundingBox SRS="EPSG:4326"
+                    minx="-10" miny="-10" maxx="10" maxy="10" />
+        <Style>
+          <Name>default</Name>
+          <Title>default</Title>
+          <LegendURL width="65" height="21">
+             <Format>image/png</Format>
+             <OnlineResource xmlns:xlink="http://www.w3.org/1999/xlink" xlink:type="simple" xlink:href="http://example.com/ogc?version=1.1.1&amp;service=WMS&amp;request=GetLegendGraphic&amp;layer=postgis-point&amp;format=image/png&amp;STYLE=default"/>
+          </LegendURL>
+        </Style>
+        <ScaleHint min="2.49451424214819" max="249.451424214819" />
+      </Layer>
+      <Layer queryable="1" opaque="0" cascaded="0">
+        <Name>shp-point-auto</Name>
+        <Title>Layer éàè</Title>
+        <SRS>EPSG:4326</SRS>
+        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
+        <BoundingBox SRS="EPSG:4326"
+                    minx="-10" miny="-10" maxx="10" maxy="10" />
+      </Layer>
+      <Layer queryable="1" opaque="0" cascaded="0">
+        <Name>shp-point</Name>
+        <Title>Layer éàè</Title>
+        <SRS>EPSG:4326</SRS>
+        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
+        <BoundingBox SRS="EPSG:4326"
+                    minx="-10" miny="-10" maxx="10" maxy="10" />
+        <Style>
+          <Name>default</Name>
+          <Title>default</Title>
+          <LegendURL width="65" height="21">
+             <Format>image/png</Format>
+             <OnlineResource xmlns:xlink="http://www.w3.org/1999/xlink" xlink:type="simple" xlink:href="http://example.com/ogc?version=1.1.1&amp;service=WMS&amp;request=GetLegendGraphic&amp;layer=shp-point&amp;format=image/png&amp;STYLE=default"/>
+          </LegendURL>
+        </Style>
+        <ScaleHint min="2.49451424214819" max="249.451424214819" />
+      </Layer>
+      <Layer queryable="1" opaque="0" cascaded="0">
+        <Name>wfs-point</Name>
+        <Title>Layer éàè</Title>
+        <SRS>EPSG:4326</SRS>
+        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
+        <BoundingBox SRS="EPSG:4326"
+                    minx="-10" miny="-10" maxx="10" maxy="10" />
+        <ScaleHint min="2.49451424214819" max="249.451424214819" />
+      </Layer>
     </Layer>
     <Layer queryable="1" opaque="0" cascaded="0">
         <Name>postgis-line-auto</Name>
@@ -134,23 +198,6 @@ class TestGetCapabilities(TestXML):
                     minx="-10" miny="-10" maxx="10" maxy="10" />
     </Layer>
     <Layer queryable="1" opaque="0" cascaded="0">
-        <Name>postgis-point</Name>
-        <Title>Layer éàè</Title>
-        <SRS>EPSG:4326</SRS>
-        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
-        <BoundingBox SRS="EPSG:4326"
-                    minx="-10" miny="-10" maxx="10" maxy="10" />
-        <Style>
-          <Name>default</Name>
-          <Title>default</Title>
-          <LegendURL width="65" height="21">
-             <Format>image/png</Format>
-             <OnlineResource xmlns:xlink="http://www.w3.org/1999/xlink" xlink:type="simple" xlink:href="http://example.com/ogc?version=1.1.1&amp;service=WMS&amp;request=GetLegendGraphic&amp;layer=postgis-point&amp;format=image/png&amp;STYLE=default"/>
-          </LegendURL>
-        </Style>
-        <ScaleHint min="2.49451424214819" max="249.451424214819" />
-    </Layer>
-    <Layer queryable="1" opaque="0" cascaded="0">
         <Name>postgis-line</Name>
         <Title>Layer éàè</Title>
         <SRS>EPSG:4326</SRS>
@@ -160,14 +207,6 @@ class TestGetCapabilities(TestXML):
     </Layer>
     <Layer queryable="1" opaque="0" cascaded="0">
         <Name>postgis-polygon</Name>
-        <Title>Layer éàè</Title>
-        <SRS>EPSG:4326</SRS>
-        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
-        <BoundingBox SRS="EPSG:4326"
-                    minx="-10" miny="-10" maxx="10" maxy="10" />
-    </Layer>
-    <Layer queryable="1" opaque="0" cascaded="0">
-        <Name>shp-point-auto</Name>
         <Title>Layer éàè</Title>
         <SRS>EPSG:4326</SRS>
         <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
@@ -189,23 +228,6 @@ class TestGetCapabilities(TestXML):
         <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
         <BoundingBox SRS="EPSG:4326"
                     minx="-10" miny="-10" maxx="10" maxy="10" />
-    </Layer>
-    <Layer queryable="1" opaque="0" cascaded="0">
-        <Name>shp-point</Name>
-        <Title>Layer éàè</Title>
-        <SRS>EPSG:4326</SRS>
-        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
-        <BoundingBox SRS="EPSG:4326"
-                    minx="-10" miny="-10" maxx="10" maxy="10" />
-        <Style>
-          <Name>default</Name>
-          <Title>default</Title>
-          <LegendURL width="65" height="21">
-             <Format>image/png</Format>
-             <OnlineResource xmlns:xlink="http://www.w3.org/1999/xlink" xlink:type="simple" xlink:href="http://example.com/ogc?version=1.1.1&amp;service=WMS&amp;request=GetLegendGraphic&amp;layer=shp-point&amp;format=image/png&amp;STYLE=default"/>
-          </LegendURL>
-        </Style>
-        <ScaleHint min="2.49451424214819" max="249.451424214819" />
     </Layer>
     <Layer queryable="1" opaque="0" cascaded="0">
         <Name>shp-line</Name>
@@ -230,15 +252,6 @@ class TestGetCapabilities(TestXML):
         <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
         <BoundingBox SRS="EPSG:4326"
                     minx="-10" miny="-10" maxx="10" maxy="10" />
-    </Layer>
-    <Layer queryable="1" opaque="0" cascaded="0">
-        <Name>wfs-point</Name>
-        <Title>Layer éàè</Title>
-        <SRS>EPSG:4326</SRS>
-        <LatLonBoundingBox minx="-10" miny="-10" maxx="10" maxy="10" />
-        <BoundingBox SRS="EPSG:4326"
-                    minx="-10" miny="-10" maxx="10" maxy="10" />
-        <ScaleHint min="2.49451424214819" max="249.451424214819" />
     </Layer>
   </Layer>
 </Capability>
